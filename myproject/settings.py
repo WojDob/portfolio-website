12b2ab0687cb3f3c44ccca68a2be35c4503d7262
Wojdob <wojciech.dobrychlop@gmail.com>
2020-07-31 15:22:56 +0200

    Changed secret key

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-from .secret_settings import *
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
